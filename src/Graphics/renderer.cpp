a713fc19ea664dd970ad372ef664787f01a20376
= <=>
2019-02-26 17:34:14 +0200

    Laatikko piirtyy ruudulle.

--- src/Graphics/renderer.cpp
+++ src/Graphics/renderer.cpp
@@ -15,10 +15,10 @@ void Renderer::init()
     glEnable(GL_DEPTH_TEST); // Enable depth test
     glDepthFunc(GL_LESS); // Accept fragment if it closer to the camera than the former one
     glFrontFace(GL_CW);
-//    //glFrontFace(GL_CCW);
-    glCullFace(GL_FRONT);
-//    //glCullFace(GL_BACK);
+//    glFrontFace(GL_CCW);
     glEnable(GL_CULL_FACE);
+    glCullFace(GL_FRONT);
+//    glCullFace(GL_BACK);
     //glEnable(GL_MULTISAMPLE);
 }
 
@@ -42,32 +42,34 @@ void Renderer::render(const Vertexbuffer& vb,
 //  if (pah){ glFrontFace(GL_CW); glCullFace(GL_FRONT); }
 //  else { glFrontFace(GL_CCW); glCullFace(GL_BACK); }
 //  auto h = vb.getHandle();
-  float yhyy = sin((float)lkm*0.02)*5.0;
+  float yhyy = sin((float)lkm*0.008);
 //  Log::getDebug().log("yhyy: %", std::to_string(yhyy));
   vb.bind();
   shader.bind();
   //vb.bind();
-  glm::vec3 lightPosition = glm::vec3(15.0f,25.0f,5.0f);
-  glm::vec3 eyePosition = glm::vec3(1.0f,1.0f,yhyy*1.0f);
+  glm::vec3 lightPosition = glm::vec3(10.0f,-10.0f,-10.0f);
+  glm::vec3 eyePosition = glm::vec3(5.0f,25.0f,25.0f);
 
-//  shader.setUniform("LightPosition_worldspace", glm::vec3(10.0f,50.0f,10.0f));
-//  shader.setUniform("lightPosition", eyePosition);
-//  shader.setUniform("lights[0].position", lightPosition);
-//  shader.setUniform("lights[0].color", glm::vec3(1.0f,1.0f,1.0f));
-//  shader.setUniform("lights[0].ambientCoeffience", 0.05f);
-//  shader.setUniform("lights[0].materialSpecularColor", glm::vec3(1.0f,1.0f,1.0f));
-//  shader.setUniform("lights[0].materialShininess", 70.0f);
-//  shader.setUniform("lights[0].attentuationFactor", 0.000009f);
+//  shader.setUniform("lightPosition", lightPosition);
+  shader.setUniform("cameraPosition", eyePosition);
+  shader.setUniform("lights[0].position", lightPosition);
+  shader.setUniform("lights[0].color", glm::vec3(1.0f,1.0f,1.0f));
+  shader.setUniform("lights[0].ambientCoeffience", 0.25f);
+  shader.setUniform("lights[0].materialSpecularColor", glm::vec3(1.0f,1.0f,1.0f));
+  shader.setUniform("lights[0].materialShininess", 70.0f);
+  shader.setUniform("lights[0].attentuationFactor", 0.000009f);
+  //shader.setUniform("lights[0].attentuationFactor", 0.000009f);
 
-  glm::mat4 projection = glm::perspective(glm::radians(150.0f), 16.0f / 9.0f, 0.1f, 1000.f);
+  glm::mat4 projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
   glm::mat4 view = glm::lookAt(eyePosition, glm::vec3(0.0,0.0,0.0), glm::vec3(0.0f, 1.0f,  0.0f));
-  glm::mat4 model = glm::mat4(1.0f);// glm::translate(glm::scale(glm::mat4(1.0f), glm::vec3(yhyy*0.2f)), glm::vec3(yhyy*0.2f, 1.0f, 1.0f)); // glm::mat4(yhyy*0.2f);
-//  shader.setUniform("normalMatrix", glm::inverseTranspose(glm::mat3(model)));
+  //glm::mat4 model = glm::scale(glm::mat4(1.0f), glm::vec3(5.0f)); // glm::mat4(yhyy*0.2f);
+  glm::mat4 model = glm::translate(glm::scale(glm::mat4(1.0f), glm::vec3(2.0f)), glm::vec3((-1.0f)*yhyy*5.0f,yhyy*5.0f, yhyy*1.0f)); // glm::mat4(yhyy*0.2f);
+  shader.setUniform("normalMatrix", glm::inverseTranspose(glm::mat3(model)));
 
   shader.setUniform("MVP", projection * view * model);
-//  shader.setUniform("M", model);
+  shader.setUniform("M", model);
 //  shader.setUniform("V", view);
 //  shader.setUniform("P", projection);
 
-  glDrawArrays(GL_TRIANGLES, 0, 12);
+  glDrawArrays(GL_TRIANGLES, 0, 36);
 }
--- src/Graphics/vertexbuffer.cpp
+++ src/Graphics/vertexbuffer.cpp
@@ -47,117 +47,146 @@ void Vertexbuffer::addData(const void* data, unsigned int size) const
 
 void Vertexbuffer::createExampleCube()
 {
-  std::vector<glm::vec3> vData = {glm::vec3(0.0f, 0.0f, 0.0f),
-                                  glm::vec3(0.0f, 0.0f, 1.0f),
-                                  glm::vec3(0.0f, 1.0f, 0.0f),
-                                  glm::vec3(0.0f, 1.0f, 1.0f),
-                                  glm::vec3(1.0f, 0.0f, 0.0f),
-                                  glm::vec3(1.0f, 0.0f, 1.0f),
-                                  glm::vec3(1.0f, 1.0f, 0.0f),
-                                  glm::vec3(1.0f, 1.0f, 1.0f)};
 
-  std::vector<glm::vec3> nData = {glm::vec3(0.0, 0.0, 0.0),
-                                  glm::vec3(0.0, 0.0,-1.0),
-                                  glm::vec3(0.0, 1.0, 0.0),
-                                  glm::vec3(0.0,-1.0, 0.0),
-                                  glm::vec3(1.0, 0.0, 0.0),
-                                  glm::vec3(-1.0, 0.0, 0.0)};
+  std::vector<glm::vec3> vData = {glm::vec3(1.0f , -1.0f, -1.0f),
+                                  glm::vec3(1.0f , -1.0f, 1.0f),
+                                  glm::vec3(-1.0f, -1.0f, 1.0f),
+                                  glm::vec3(-1.0f, -1.0f, -1.0f),
+                                  glm::vec3(1.0f , 1.0f, -1.0f),
+                                  glm::vec3(1.0f, 1.0f, 1.0f),
+                                  glm::vec3(-1.0f, 1.0f, 1.0f),
+                                  glm::vec3(-1.0f, 1.0f, -1.0f)};
+
+  std::vector<glm::vec3> nData = {glm::vec3(0.0f , 0.0f , -1.0f),
+                                  glm::vec3(-1.0f, -0.0f, 0.0f),
+                                  glm::vec3(0.0f, -0.0f, 1.0f),
+                                  glm::vec3(0.0f, 0.0f , 1.0f),
+                                  glm::vec3(1.0f , -0.0f, 0.0f),
+                                  glm::vec3(1.0f , 0.0f , 0.0f),
+                                  glm::vec3(0.0f , 1.0f , 0.0f),
+                                  glm::vec3(0.0f, -1.0f, 0.0f)};
+//  std::vector<glm::vec3> vData = {glm::vec3(0.0f, 0.0f, 0.0f),
+//                                  glm::vec3(0.0f, 0.0f, 1.0f),
+//                                  glm::vec3(0.0f, 1.0f, 0.0f),
+//                                  glm::vec3(0.0f, 1.0f, 1.0f),
+//                                  glm::vec3(1.0f, 0.0f, 0.0f),
+//                                  glm::vec3(1.0f, 0.0f, 1.0f),
+//                                  glm::vec3(1.0f, 1.0f, 0.0f),
+//                                  glm::vec3(1.0f, 1.0f, 1.0f)};
+
+//  std::vector<glm::vec3> nData = {glm::vec3(0.0, 0.0, 0.0),
+//                                  glm::vec3(0.0, 0.0,-1.0),
+//                                  glm::vec3(0.0, 1.0, 0.0),
+//                                  glm::vec3(0.0,-1.0, 0.0),
+//                                  glm::vec3(1.0, 0.0, 0.0),
+//                                  glm::vec3(-1.0, 0.0, 0.0)};
   // Face 1
-//  std::vector<glm::vec3> pData;
-  pData.push_back(vData[0]);
-  pData.push_back(nData[1]);
-  pData.push_back(vData[6]);
-  pData.push_back(nData[1]);
+//  f 5/1/1 1/2/1 4/3/1
   pData.push_back(vData[4]);
-  pData.push_back(nData[1]);
+  pData.push_back(nData[0]);
+  pData.push_back(vData[0]);
+  pData.push_back(nData[0]);
+  pData.push_back(vData[3]);
+  pData.push_back(nData[0]);
 
   // Face2
-  pData.push_back(vData[0]);
-  pData.push_back(nData[1]);
-  pData.push_back(vData[2]);
-  pData.push_back(nData[1]);
-  pData.push_back(vData[6]);
-  pData.push_back(nData[1]);
+//  f 5/1/1 4/3/1 8/4/1
+  pData.push_back(vData[4]);
+  pData.push_back(nData[0]);
+  pData.push_back(vData[3]);
+  pData.push_back(nData[0]);
+  pData.push_back(vData[7]);
+  pData.push_back(nData[0]);
 
   // Face3
-  pData.push_back(vData[0]);
-  pData.push_back(nData[5]);
-  pData.push_back(vData[3]);
-  pData.push_back(nData[5]);
+//  f 3/5/2 7/6/2 8/7/2
   pData.push_back(vData[2]);
-  pData.push_back(nData[5]);
+  pData.push_back(nData[1]);
+  pData.push_back(vData[6]);
+  pData.push_back(nData[1]);
+  pData.push_back(vData[7]);
+  pData.push_back(nData[1]);
 
   // Face4
-  pData.push_back(vData[0]);
-  pData.push_back(nData[5]);
-  pData.push_back(vData[1]);
-  pData.push_back(nData[5]);
+//  f 3/5/2 8/7/2 4/8/2
+  pData.push_back(vData[2]);
+  pData.push_back(nData[1]);
+  pData.push_back(vData[7]);
+  pData.push_back(nData[1]);
   pData.push_back(vData[3]);
-  pData.push_back(nData[5]);
+  pData.push_back(nData[1]);
 
   // Face5
+//  f 2/9/3 6/10/3 3/5/3
+  pData.push_back(vData[1]);
+  pData.push_back(nData[2]);
+  pData.push_back(vData[5]);
+  pData.push_back(nData[2]);
   pData.push_back(vData[2]);
-  pData.push_back(nData[2]);
-  pData.push_back(vData[7]);
-  pData.push_back(nData[2]);
-  pData.push_back(vData[6]);
   pData.push_back(nData[2]);
 
   // Face6
+//  f 6/10/4 7/6/4 3/5/4
+  pData.push_back(vData[5]);
+  pData.push_back(nData[3]);
+  pData.push_back(vData[6]);
+  pData.push_back(nData[3]);
   pData.push_back(vData[2]);
-  pData.push_back(nData[2]);
-  pData.push_back(vData[3]);
-  pData.push_back(nData[2]);
-  pData.push_back(vData[7]);
-  pData.push_back(nData[2]);
+  pData.push_back(nData[3]);
 
   // Face7
+//  f 1/2/5 5/1/5 2/9/5
+  pData.push_back(vData[0]);
+  pData.push_back(nData[4]);
   pData.push_back(vData[4]);
   pData.push_back(nData[4]);
-  pData.push_back(vData[6]);
-  pData.push_back(nData[4]);
-  pData.push_back(vData[7]);
+  pData.push_back(vData[1]);
   pData.push_back(nData[4]);
 
   // Face8
+//  f 5/1/6 6/10/6 2/9/6
   pData.push_back(vData[4]);
-  pData.push_back(nData[4]);
-  pData.push_back(vData[7]);
-  pData.push_back(nData[4]);
+  pData.push_back(nData[5]);
   pData.push_back(vData[5]);
-  pData.push_back(nData[4]);
+  pData.push_back(nData[5]);
+  pData.push_back(vData[1]);
+  pData.push_back(nData[5]);
 
   // Face9
-  pData.push_back(vData[0]);
-  pData.push_back(nData[3]);
+//  f 5/1/7 8/11/7 6/10/7
   pData.push_back(vData[4]);
-  pData.push_back(nData[3]);
+  pData.push_back(nData[6]);
+  pData.push_back(vData[7]);
+  pData.push_back(nData[6]);
   pData.push_back(vData[5]);
-  pData.push_back(nData[3]);
+  pData.push_back(nData[6]);
 
   // Face10
-  pData.push_back(vData[0]);
-  pData.push_back(nData[3]);
+ // f 8/11/7 7/12/7 6/10/7
+  pData.push_back(vData[7]);
+  pData.push_back(nData[6]);
+  pData.push_back(vData[6]);
+  pData.push_back(nData[6]);
   pData.push_back(vData[5]);
-  pData.push_back(nData[3]);
-  pData.push_back(vData[1]);
-  pData.push_back(nData[3]);
+  pData.push_back(nData[6]);
 
   // Face11
+//  f 1/2/8 2/9/8 3/13/8
+  pData.push_back(vData[0]);
+  pData.push_back(nData[7]);
   pData.push_back(vData[1]);
-  pData.push_back(nData[0]);
-  pData.push_back(vData[5]);
-  pData.push_back(nData[0]);
-  pData.push_back(vData[7]);
-  pData.push_back(nData[0]);
+  pData.push_back(nData[7]);
+  pData.push_back(vData[2]);
+  pData.push_back(nData[7]);
 
   // Face12
-  pData.push_back(vData[1]);
-  pData.push_back(nData[0]);
-  pData.push_back(vData[7]);
-  pData.push_back(nData[0]);
+//  f 1/2/8 3/13/8 4/14/8
+  pData.push_back(vData[0]);
+  pData.push_back(nData[7]);
+  pData.push_back(vData[2]);
+  pData.push_back(nData[7]);
   pData.push_back(vData[3]);
-  pData.push_back(nData[0]);
+  pData.push_back(nData[7]);
 //  f  1//2  7//2  5//2
 //f  1//2  3//2  7//2
 //f  1//6  4//6  3//6
